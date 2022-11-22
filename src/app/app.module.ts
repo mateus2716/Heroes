779d9bac472d750f1bc48fa658e1f13d90f61afe
mateus2716 <mateusdias112@gmail.com>
2022-11-22 20:37:28 -0300

    Criação dos components
    Criação dos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { InputComponent } from './input/input.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InputComponent,
+    GalleryComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
